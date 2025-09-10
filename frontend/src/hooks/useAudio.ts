56ec3005cc081b8c84bcb034ac1d51ae7ba757fb
Ilya Makarov <devmakarov67@gmail.com>
2025-09-10 13:54:54 +0300

    fix: updated path for audio

--- frontend/src/hooks/useAudio.ts
+++ frontend/src/hooks/useAudio.ts
@@ -10,22 +10,21 @@ export enum EAudio {
 
 const useAudio = () => {
   const audio = useRef({
-    [EAudio.Miss]: new Audio("/src/assets/audio/shot.mp3"),
-    [EAudio.Hit]: new Audio("/src/assets/audio/hit.mp3"),
-    [EAudio.Won]: new Audio("/src/assets/audio/won.mp3"),
-    [EAudio.Lost]: new Audio("/src/assets/audio/lost.mp3"),
-    [EAudio.GameStarted]: new Audio("/src/assets/audio/game-started.mp3"),
+    [EAudio.Miss]: new Audio("/audio/shot.mp3"),
+    [EAudio.Hit]: new Audio("/audio/hit.mp3"),
+    [EAudio.Won]: new Audio("/audio/won.mp3"),
+    [EAudio.Lost]: new Audio("/audio/lost.mp3"),
+    [EAudio.GameStarted]: new Audio("/audio/game-started.mp3"),
   });
 
   const playAudio = (key: EAudio) => {
-    audio.current[key].currentTime = 0;
-    audio.current[key].volume = 0.3;
-    audio.current[key].play();
+    const a = audio.current[key];
+    a.currentTime = 0;
+    a.volume = 0.3;
+    a.play().catch((e) => console.log("Audio play error:", e));
   };
 
-  return {
-    playAudio,
-  };
+  return { playAudio };
 };
 
 export default useAudio;
