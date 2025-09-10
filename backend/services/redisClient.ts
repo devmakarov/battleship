55802c49335f3b0792ce4e01c45765b9101818be
Ilya Makarov <devmakarov67@gmail.com>
2025-09-10 13:15:42 +0300

    fix: redisClient.ts updated to a public url

--- backend/services/redisClient.ts
+++ backend/services/redisClient.ts
@@ -2,12 +2,8 @@ import Redis from "ioredis";
 
 const isProduction = process.env.NODE_ENV === "production";
 
-console.log('isProduction', isProduction)
-const redisUrl = isProduction
-    ? process.env.REDIS_URL!
-    : process.env.REDIS_URL || "redis://127.0.0.1:6379";
-
-const redis = new Redis(redisUrl, {  });
+console.log('REDIS_PUBLIC_URL', process.env.REDIS_PUBLIC_URL);
+const redis = new Redis(process.env.REDIS_PUBLIC_URL ? process.env.REDIS_PUBLIC_URL : "redis://127.0.0.1:6379");
 
 redis.on("error", (err) => {
     console.error("Redis error:", err);
